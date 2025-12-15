e8fc9f102a140d6e507fb5f9c1c535b53dd62dd0
SoumenJ25 <soumen.jana@cognizant.com>
2025-12-15 17:51:35 +0530

    fix: update registration validation logic

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -1,12 +1,15 @@
 const { body } = require("express-validator");
 
 exports.registerValidator = [
-  body("username").trim().isLength({ min: 2 }).withMessage('Username must be at least 2 characters'),
+  body("username")
+    .trim()
+    .notEmpty().withMessage("Username is required"),
   body('email')
+    .notEmpty().withMessage("Email is required")
     .isEmail().withMessage('Invalid email address')
     .normalizeEmail(),
   body('password')
+    .notEmpty().withMessage("Password is required")
     .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
-    .matches(/\d/).withMessage('Password must contain a number')
-    .matches(/[A-Z]/).withMessage('Password must contain an uppercase letter')
+    .isAlphanumeric().withMessage("Password must be alphanumeric")
 ];
